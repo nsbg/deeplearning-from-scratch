6555a85ae15339b8d4b2de9eaf325f3a674884e3
Bo geum Kim <epdlsemgks872@gmail.com>
2023-02-13 20:21:52 +0900

    Add 밑시딥 1권 ch05

---
+++ 1권/common/layers.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+class ReLU:
+    def __init__(self):
+        self.mask = None
+        
+    def forward(self, x):
+        self.mask = (x <= 0)
+        
+        out = x.copy()
+        out[self.mask] = 0
+        
+        return out
+    
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+        
+        return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+        
+    def forward(self, x):
+        out = 1/(1+np.exp(-x))
+        
+        self.out = out
+        
+        return out
+    
+    def backward(self, dout):
+        dx = dout*(1.0-self.out)*self.out
+        
+        return dx
+    
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 손실함수
+        self.y = None    # softmax의 출력
+        self.t = None    # 정답 레이블(원-핫 인코딩 형태)
+        
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        if self.t.size == self.y.size: # 정답 레이블이 원-핫 인코딩 형태일 때
+            dx = (self.y - self.t) / batch_size
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
+        
+        return dx
